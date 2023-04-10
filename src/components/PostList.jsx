1fb2c25a14b9c2f23e68ff251b28fcd574915209
1MarcoAntonio <xfiller93@gmail.com>
2023-04-10 18:24:18 -0300

    first commit

---
+++ src/components/PostList.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
+import Post from "./Post";
+import classes from "./PostList.module.css";
+import Modal from "./Modal";
+
+function PostList({ isPosting, onStopPosting }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
+          />
+        </Modal>
+      )}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
+  );
+}
+
+export default PostList;
